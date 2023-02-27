3735cf8d5af2ff2b109ca7b49aaa2712a3bcabfe
Parvez Sohail <66157611+Parvez13@users.noreply.github.com>
2023-02-27 14:40:41 +0530

    Add files via upload

---
+++ insurance/config.py
@@ -0,0 +1,12 @@
+import pymongo
+import json as pd
+from dataclasses import dataclass
+import os
+
+@dataclass
+class EnvironmentVariable():
+    mongo_db_url:str = os.getenv('MONGO_DB_URL')
+
+env_var = EnvironmentVariable()
+mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
+TARGET_COLUMN = 'expenses'
